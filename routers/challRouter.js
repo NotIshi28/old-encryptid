392b17d968d2e7e853ede93ab96a01c728b37711
Ishit Rastogi <ishitrastogi@gmail.com>
2024-10-02 18:01:07 +0530

    submit cryptic answers

--- routers/challRouter.js
+++ routers/challRouter.js
@@ -1,18 +1,49 @@
 const router = require('express').Router();
 const Crypt = require('../models/CryptSchema.js');
 const Ctf = require('../models/CtfSchema.js');
-
+const User = require('../models/userSchema.js');
 
 router.get('/cryptic', async (req, res) => {
-    let cryptLvls = await Crypt.find({});
+    let cryptLvls = await Crypt.find({}).sort({lvlNo: 1});
     console.log(cryptLvls);
-    lvls = cryptLvls.length;
-    
+    let lvls = cryptLvls;
+
     res.render('cryptic', { user: req.user, lvls: lvls})
 });
 
-router.get('/ctf', (req, res) => {
-    res.render('ctf', { user: req.user })
+router.get('/cryptic/:lvlNo', async (req, res) => {
+    let lvlNo = req.params.lvlNo;
+    let cryptLvl = await Crypt.findOne({lvlNo: lvlNo});
+    console.log(cryptLvl);
+    res.render('cryptLvl', { user: req.user, cryptLvl: cryptLvl})
+})
+
+router.post('/cryptic/:lvlNo', async (req, res) => {
+    let lvlNo = req.params.lvlNo;
+    let cryptLvl = await Crypt.findOne({lvlNo: lvlNo});
+    console.log(req.body.answer)
+    if(req.body.answer == cryptLvl.answer){
+        console.log("correct");
+        res.redirect('/challenges/cryptic');
+    }
+    else{
+        console.log("GALT JAWAB");
+        res.render('cryptLvl', { user: req.user, cryptLvl: cryptLvl})
+    }
+})
+
+router.get('/ctf', async (req, res) => {
+    let ctfLvls = await Ctf.find({});
+    
+    let lvls = ctfLvls;
+    res.render('ctf', { user: req.user, lvls : lvls})
+});
+
+router.get('/ctf/:lvlNo', async (req, res) => {
+    let lvlNo = req.params.lvlNo;
+    let ctfLvl = await Ctf.findOne({lvlNo: lvlNo});
+    console.log(ctfLvl);
+    res.render('ctfLvl', { user: req.user, ctfLvl: ctfLvl})
 });
 
 
